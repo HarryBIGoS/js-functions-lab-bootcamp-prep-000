cd272bada8405570fb53b9894005760117d25661
Harry Bigos <hrbigos@gmail.com>
2018-08-21 01:09:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
+function holdiayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
+}
+
