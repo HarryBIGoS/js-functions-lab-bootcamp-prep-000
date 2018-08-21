cc919b678ab0944ab90bc31e796860674bbf031a
Harry Bigos <hrbigos@gmail.com>
2018-08-21 01:08:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,3 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-
-3. Write a function named `happyHolidayTo`. This function should accept two parameters, the holiday you want to wish them well for, and the name of the person you're wishing well. This function should return the string `` `Happy ${holiday}, ${name}!` ``
